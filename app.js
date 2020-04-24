086b6adfa105cb47414d820f5f18a33fe129b8f0
Josue Villanueva <idjevm@gmail.com>
2020-04-24 07:20:23 -0400

    "new"

---
+++ app.js
@@ -0,0 +1,29 @@
+// app.js
+
+const express = require('express')
+const connectDB = require('./config/db'); 
+const cors = require('cors'); 
+
+// routes
+books = require('./routes/api/books');
+
+// Start app
+const app = express(); 
+
+// Connect to MongoDB
+connectDB();
+
+// cors
+app.use(cors({ origin: true, credentials: true }));
+
+// Init Middleware
+app.use(express.json({ extended: false })); 
+
+app.get('/', (req, res) => res.send('Hello JV!'));
+
+// use Routes
+app.use('/api/books', books);
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server is now running on port: ${port}`));
