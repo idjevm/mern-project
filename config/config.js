cef533c359505036655076a25296e6b5f47f599f
Josue Villanueva <idjevm@gmail.com>
2020-04-28 12:31:47 -0400

    added now.json to deploy in verce & updated package json to deploy in heroku

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-module.exports = {
+module.exports = { // Not being used atm but left here for reference if needed 
     port: process.env.PORT || 5000,
     secret: process.env.SECRET || "whatever your secret is",
     db: process.env.MONGO_DB_URI
